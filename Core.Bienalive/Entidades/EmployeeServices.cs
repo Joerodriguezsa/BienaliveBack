1b7258c1be2fcff1ce92860f39c58fe3d18b9365
John Rodriguez Santana <johned32@hotmail.com>
2026-01-12 11:03:15 -0500

    Create EmployeeServices.cs

---
+++ Core.Bienalive/Entidades/EmployeeServices.cs
@@ -0,0 +1,27 @@
+﻿using Utilitarios.Entidades;
+
+namespace Core.Bienalive.Entidades
+{
+    public class EmployeeServices : EntidadBase
+    {
+        /// <summary>
+        /// Llave primaria de la entidad.
+        /// </summary>
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Identificador del empleado (User con rol empleado).
+        /// </summary>
+        public long TeamMemberId { get; set; }
+
+        /// <summary>
+        /// Identificador del servicio que el empleado puede prestar.
+        /// </summary>
+        public long ServiceId { get; set; }
+
+        /// <summary>
+        /// Indica si el empleado está activo para este servicio.
+        /// </summary>
+        public bool Active { get; set; } = true;
+    }
+}
